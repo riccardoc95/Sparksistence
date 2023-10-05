2da4eeeef8ebc221bd76f7388cc3d92b9656c0aa
Riccardo Ceccaroni <50733776+riccardoc95@users.noreply.github.com>
2023-10-05 16:19:53 +0200

    updates

--- scripts/pyspark.sh
+++ scripts/pyspark.sh
@@ -9,7 +9,7 @@ export PYSPARK_DRIVER_PYTHON="/lustre/home/rceccaroni/.conda/envs/python/bin/pyt
 export PYSPARK_PYTHON="/lustre/home/rceccaroni/.conda/envs/python/bin/python"
 export PYTHONPATH="/lustre/home/rceccaroni/.conda/envs/python/bin/python"
 export PYSPARK_DRIVER_PYTHON="jupyter"
-export PYSPARK_DRIVER_PYTHON_OPTS="notebook --notebook-dir=$(dirname "$THIS_PATH") --no-browser --port=9999"
+export PYSPARK_DRIVER_PYTHON_OPTS="notebook --notebook-dir=${USER}/Sparksistence/ --no-browser --port=9999"
 
 
 command="bin/pyspark --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=/lustre/home/rceccaroni/.conda/envs/python/bin/python"
